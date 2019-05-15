26a316c5aee8a9c6567dd5bdb462e3ec5643d299
uri even ezra <uevenezra@gmail.com>
2019-05-15 11:02:51 +0000

    first commit - subsribe to db(notification) send to RabbitMQ server

---
+++ index.js
@@ -0,0 +1,62 @@
+const amqp = require('amqplib');
+const createSubscriber  = require ('pg-listen'); 
+
+const send = (queue,msg) => amqp.connect('amqp://localhost').then(function(conn) {
+  return conn.createConfirmChannel().then(function(ch) {
+
+    var ok = ch.assertQueue(queue, {durable: false});
+
+    return ok.then(async function(_qok) {
+      // NB: `sentToQueue` and `publish` both return a boolean
+      // indicating whether it's OK to send again straight away, or
+      // (when `false`) that you should wait for the event `'drain'`
+      // to fire before writing again. We're just doing the one write,
+      // so we'll ignore it.
+      ch.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
+      await ch.waitForConfirms();
+      console.log(" [x] Sent '%s'", msg);
+      return ch.close();
+    });
+  }).finally(function() { conn.close(); });
+}).catch(console.warn);
+
+//import { databaseURL } from "./config"
+const databaseURL = "postgres://admin:cbtadmin@192.9.200.102:5432/mymes";
+// Accepts the same connection config object that the "pg" package would take
+const subscriber = createSubscriber({ connectionString: databaseURL })
+
+subscriber.notifications.on("notifications", (payload) => {
+  // Payload as passed to subscriber.notify() (see below)
+  console.log("Received notification :", payload)
+  send('hello',payload)
+})
+
+subscriber.events.on("error", (error) => {
+  console.error("Fatal database connection error:", error)
+  process.exit(1)
+})
+
+process.on("exit", () => {
+  subscriber.close()
+})
+// connect to rabbitMQ
+
+
+const connect = async ()=> {
+    await subscriber.connect()
+    await subscriber.listenTo("notifications")
+  }
+  
+const sendToDB = async ()=> {
+  await subscriber.notify({
+    id: 1,
+    greeting: "Hey, buddy.",
+    timestamp: Date.now()
+  })
+}
+
+connect() 
+console.log('Subscribed to DB')
+
+
+
